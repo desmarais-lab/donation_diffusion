ede503e648ea0a9b28353f82530b4f8eb2f77ead
Fridolin Linder <fridolin.linder@gmail.com>
2017-09-20 10:51:40 -0400

    minor change in etl

--- etl/main_etl.sh
+++ etl/main_etl.sh
@@ -13,12 +13,13 @@ rm itpas2.txt
 ## 2018
 wget ftp://ftp.fec.gov/FEC/2018/pas218.zip
 unzip pas218.zip
-rm pas218.zip
 python psv_to_csv.py itpas2.txt pas2_header_file.csv ../data/2018_pac_contributions.csv
-rm itpas2.txt
-rm pas2_header_file.csv
 
 # Concatenate the datasets
 python concat_csvs.py ../data/*_pac_contributions.csv ../data/pac_contributions.csv
 
+# Clean up
+rm pas218.zip
+rm itpas2.txt
+rm pas2_header_file.csv
 rm ../data/*_pac_contributions.csv
