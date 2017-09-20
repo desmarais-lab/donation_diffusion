359e9876cb021114317eab011a63dd02281c9443
Fridolin Linder <fridolin.linder@gmail.com>
2017-09-20 12:23:46 -0400

    join donation files with candidate info

--- etl/README.md
+++ etl/README.md
@@ -3,6 +3,8 @@
 `main_etl.sh` does the following:
 * Download committee contribution data for 2015-2016 and 2017-2018 + header
     files
+* Downloads candidate master files for same years + headers
 * Parses the pipe separated files to csv
 * Adds headers
+* joins candidate and donation files by candidate id
 * Concatenates them to a single file
--- etl/main_etl.sh
+++ etl/main_etl.sh
@@ -1,25 +1,58 @@
-# Header file 
+# Committee header file
 wget http://classic.fec.gov/finance/disclosure/metadata/pas2_header_file.csv
+# Candidate header file
+wget http://classic.fec.gov/finance/disclosure/metadata/cn_header_file.csv
 
 # Donation data 
 
 ## 2016
+
+### Donation data
 wget ftp://ftp.fec.gov/FEC/2016/pas216.zip
 unzip pas216.zip
 rm pas216.zip
-python psv_to_csv.py itpas2.txt pas2_header_file.csv ../data/2016_pac_contributions.csv
+python psv_to_csv.py itpas2.txt pas2_header_file.csv 2016_pac_contributions.csv
 rm itpas2.txt
+
+### Candidate data
+wget ftp://ftp.fec.gov/FEC/2016/cn16.zip
+unzip cn16.zip
+rm cn16.zip
+python psv_to_csv.py cn.txt cn_header_file.csv 2016_candidates.csv
+rm cn.txt
+
+### Join donation and candidate data
+python join_donation_candidate.py 2016_pac_contributions.csv \
+    2016_candidates.csv 2016_pac_contributions.csv
+
+rm 2016_candidates.csv
 
 ## 2018
+
+### Donation Data
 wget ftp://ftp.fec.gov/FEC/2018/pas218.zip
 unzip pas218.zip
-python psv_to_csv.py itpas2.txt pas2_header_file.csv ../data/2018_pac_contributions.csv
+rm pas218.zip
+python psv_to_csv.py itpas2.txt pas2_header_file.csv 2018_pac_contributions.csv
+rm itpas2.txt
+
+### Candidate data
+wget ftp://ftp.fec.gov/FEC/2018/cn18.zip
+unzip cn18.zip
+rm cn18.zip
+python psv_to_csv.py cn.txt cn_header_file.csv 2018_candidates.csv
+rm cn.txt
+
+### Join donation and candidate data
+python join_donation_candidate.py 2018_pac_contributions.csv \
+    2018_candidates.csv 2018_pac_contributions.csv
+
+rm 2018_candidates.csv
 
 # Concatenate the datasets
-python concat_csvs.py ../data/*_pac_contributions.csv ../data/pac_contributions.csv
+python concat_csvs.py *_pac_contributions.csv ../data/pac_contributions.csv
 
 # Clean up
-rm pas218.zip
-rm itpas2.txt
 rm pas2_header_file.csv
-rm ../data/*_pac_contributions.csv
+rm cn_header_file.csv
+rm *_pac_contributions.csv
