0153ff41d597da89538ce22c4cf8a32b5100fccc
flinder <fridolin.linder@gmail.com>
2017-11-01 08:21:02 -0400

    data preprocessing for netinf

--- analysis/network_inference.R
+++ analysis/network_inference.R
@@ -1,29 +1,38 @@
 library(tidyverse)
-devtools::install_github('desmarais-lab/NetworkInference')
+devtools::install('~/projects/NetworkInference/')
 library(NetworkInference)
 library(microbenchmark)
 
-parse_date <- function(x) {
-    x = as.character(x)
-    l = sapply(strsplit(x, ''), length)
-    year = substr(x, l-3, l)
-    day = substr(x, l-5, l-4)
-    month = substr(x, 1, l-6)
-    return(as.Date(paste(year, month, day, sep = '/')))
-}
+df <- read_csv('../data/EL_14.csv') %>%
+    filter(!is.element(tran_tp, 
+                       c('19', '24A', '24C', '24E', '24F', '24N', '29')),
+           amt > 0, 
+           !is.na(amt)) %>%
+    mutate(date = as.Date(date, '%m/%d/%Y'))
+    
+# Subset the data. Remove:
+# - Recipients with less than 2 unique donors
+# - Donors that give to only one candidate
+recip_smry <- group_by(df, recip) %>%
+    summarize(n_donors = length(unique(donor)))
 
-df <- read_csv('../data/contributions.csv')
+donor_smry <- group_by(df, donor) %>%
+    summarize(n_recips = length(unique(recip)))
+
+dat <- left_join(df, recip_smry, by=c('recip')) %>%
+    left_join(donor_smry, by=c('donor')) %>%
+    filter(n_donors > 1, n_recips > 1)
 
 
-
-    mutate(date = as.integer(parse_date(TRANSACTION_DT))) %>%
-    group_by(CMTE_ID, CAND_ID) %>%
-    summarize(date = min(date))
-
-cascades <- as_cascade_long(df, cascade_node_name = 'CMTE_ID', 
+# Donor types
+length(unique(dat$donor))
+length(unique(dat$recip))
+    
+cascades <- as_cascade_long(dat, cascade_node_name = 'donor', 
                             event_time = 'date', 
-                            cascade_id = 'CAND_ID',
-                            node_names = unique(df$CMTE_ID))
+                            cascade_id = 'recip',
+                            node_names = unique(dat$donor))
+
 smry <- summary(cascades) 
 res <- netinf(cascades, n_edges = 100, lambda = 1)
 save(res, 'diffnet.RData')
