bf7828303b8fa35fb221ff9072979ee061488115
flinder <fridolin.linder@gmail.com>
2017-11-10 10:40:04 -0500

    update

--- analysis/network_inference.R
+++ analysis/network_inference.R
@@ -7,21 +7,35 @@ df <- read_csv('../data/EL_14.csv') %>%
     filter(!is.element(tran_tp, 
                        c('19', '24A', '24C', '24E', '24F', '24N', '29')),
            amt > 0, 
-           !is.na(amt)) %>%
-    mutate(date = as.Date(date, '%m/%d/%Y'))
-    
+           !is.na(amt), !is.na(date), !is.na(donor_tp), !is.na(recip_tp)) %>%
+    mutate(date = as.Date(date, '%m/%d/%Y')) %>%
+    filter(date > as.Date('2013-01-01')) %>%
+    group_by(donor, recip) %>%
+    arrange(date) %>%
+    summarize(amt = amt[1], date = date[1], donor_tp = donor_tp[1],
+              recip_tp = recip_tp[1]) 
+
 # Subset the data. Remove:
 # - Recipients with less than 2 unique donors
 # - Donors that give to only one candidate
 recip_smry <- group_by(df, recip) %>%
-    summarize(n_donors = length(unique(donor)))
+    summarize(n_donors = length(unique(donor))) %>%
+    arrange(desc(n_donors)) %>% 
+    head(1000)
 
 donor_smry <- group_by(df, donor) %>%
-    summarize(n_recips = length(unique(recip)))
+    summarize(n_recips = length(unique(recip))) %>%
+    arrange(desc(n_recips)) %>%
+    head(4000)
 
 dat <- left_join(df, recip_smry, by=c('recip')) %>%
     left_join(donor_smry, by=c('donor')) %>%
-    filter(n_donors > 1, n_recips > 1)
+    filter(!is.na(n_donors), !is.na(n_recips), n_donors > 1, n_recips > 1)
+
+a = group_by(dat, recip) %>%
+    summarize(n_donors = length(unique(donor)))
+b = group_by(dat, donor) %>%
+    summarize(n_recips = length(unique(recip)))
 
 
 # Donor types
