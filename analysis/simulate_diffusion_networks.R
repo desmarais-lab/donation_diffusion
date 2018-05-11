87ecbcf7cb6cd63784ae4d7b5e961b0502ac7f09
flinder <fridolin.linder@gmail.com>
2018-05-11 09:44:34 -0400

    fixed data path in simulate_diffusion_networks

--- analysis/simulate_diffusion_networks.R
+++ analysis/simulate_diffusion_networks.R
@@ -2,18 +2,25 @@
 library(ergm)
 
 # read in ERGM objects
-load("data/ergm_results/ergm_results_threshold_8_pval_0.025.RData")
+load("../data/ergm_results/ergm_results_threshold_8_pval_0.025.RData")
 
 set.seed(12345)
 
 # simulate networks from spatial ERGM
-spatial.networks <- simulate(simple.homophily.ergm,nsim=100,control=control.simulate.ergm(MCMC.interval=10000000,MCMC.burnin=10000000))
+spatial.networks <- simulate(simple.homophily.ergm, nsim=100,
+                             control=control.simulate.ergm(
+                                 MCMC.interval=10000000,
+                                 MCMC.burnin=10000000))
 
 # simulate networks from directional ERGM, take 1000
-directional.networks <- simulate(ideological.hierarchy.ergm,nsim=100,control=control.simulate.ergm(MCMC.interval=10000000,MCMC.burnin=10000000))
+directional.networks <- simulate(ideological.hierarchy.ergm, nsim=100,
+                                 control=control.simulate.ergm(
+                                     MCMC.interval=10000000,
+                                     MCMC.burnin=10000000))
 
 # save simulation results
-save(list=c("spatial.networks","directional.networks"),file="ergm_simulation_results.RData")
+save(list=c("spatial.networks","directional.networks"), 
+     file="ergm_simulation_results.RData")
 
 # these plots should look like white noise if the simulations converged
 library(sna)
