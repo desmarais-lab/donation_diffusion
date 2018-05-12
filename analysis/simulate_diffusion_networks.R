74ff5951f9b4060e9dd6666390b42df355a8683e
bdesmarais <bdesmarais@psu.edu>
2018-05-12 06:48:24 -0400

    removed plots

--- analysis/simulate_diffusion_networks.R
+++ analysis/simulate_diffusion_networks.R
@@ -22,13 +22,6 @@ directional.networks <- simulate(ideological.hierarchy.ergm, nsim=100,constraint
 save(list=c("spatial.networks","directional.networks"), 
      file="ergm_simulation_results.RData")
 
-# these plots should look like white noise if the simulations converged
-library(sna)
-par(mfrow=c(2,1))
-plot(gden(spatial.networks),type="l")
-plot(gden(directional.networks),type="l")
-
-
 # access the adjacency matrix from a network
 # amat <- directional.networks[[1]][,]
 # see vertex ids
