865fc54a7d2cbe05efbf63a9438caa5728e7d112
flinder <fridolin.linder@gmail.com>
2018-08-16 10:04:48 +0200

    analyis code for new cascade simulation

--- 10_interpret_cascade_simulation.R
+++ 10_interpret_cascade_simulation.R
@@ -3,6 +3,11 @@ library(sna)
 library(igraph)
 library(NetworkInference)
 library(boxr)
+library(yaml)
+
+
+config = yaml.load_file('0_config.yml')
+LOCAL_DATA = config$LOCAL_DATA
 
 #devtools::install_github('flinder/flindR')
 pe = flindR::plot_elements()
@@ -11,23 +16,23 @@ SIM_RES_DIR = 'data/cascade_simulation_results/'
 
 ## Load all simulation results
 
-outfiles = list.files(SIM_RES_DIR, patter = '*.RData')
-i = 1
-for(f in outfiles) {
-    cat(i, f, '\n')
-    if(f == 'compiled_results.RData') next
-    load(paste0('../data/cascade_simulation_results/', f))
-    if(i == 1) out = do.call(rbind, results)
-    else out = rbind(out, do.call(rbind, results))
-    i = i + 1 
-}
-
-simulation_results = tbl_df(out)
-save(simulation_results, 
-     file = '../data/cascade_simulation_results/compiled_results.RData')
-
-stop()
-#load('../data/cascade_simulation_results/compiled_results.RData')
+#outfiles = list.files(SIM_RES_DIR, patter = '*.RData')
+#i = 1
+#for(f in outfiles) {
+#    cat(i, f, '\n')
+#    if(f == 'compiled_results.RData') next
+#    load(paste0('data/cascade_simulation_results/', f))
+#    if(i == 1) out = do.call(rbind, results)
+#    else out = rbind(out, do.call(rbind, results))
+#    i = i + 1 
+#}
+#
+#simulation_results = tbl_df(out)
+#save(simulation_results, 
+#     file = 'data/cascade_simulation_results/compiled_results.RData')
+#
+#stop()
+load('data/cascade_simulation_results/compiled_results.RData')
 cutoff_time = 17166
 
 
@@ -37,6 +42,7 @@ cutoff_time = 17166
 load('data/casc_sim_data.RData')
 
 # Before restriction
+donation_cascades = casc_sim_data$donation_cascades
 
 ## Number of candidates completely dropped
 candidates = names(donation_cascades$cascade_nodes)
@@ -83,20 +89,26 @@ simulation_cut = group_by(simulation_results, network_type, proportion_observed,
 # Number of donations by ideology, incumbency, network type and proportion observed
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++   
 
+if(!is.null(LOCAL_DATA)) {
+    nominate_data = read_csv(paste0(LOCAL_DATA, 'nominate_prez_data.csv'))
+    candidate_meta_data = read_csv(paste0(LOCAL_DATA, 'VLC_16_full.csv')) %>%
+        dplyr::select(Actor_ID, Incum) %>%
+        rename(candidate = Actor_ID, incumbent = Incum) %>%
+        mutate(incumbent = ifelse(incumbent == "I", 1, 0))
+} else {
+    box_auth()
+    nominate_data = box_read_csv(file_id = '311590876251') %>%
+        select(os_id, nominate_dim1) %>%
+        rename(candidate = os_id, ideology = nominate_dim1)
+    # Read 'VLC_16_full.csv' from box
+    candidate_meta_data = box_read_csv(file_id = '308095557675', fread = TRUE) %>%
+        dplyr::select(Actor_ID, Incum) %>%
+        rename(candidate = Actor_ID, incumbent = Incum) %>%
+        mutate(incumbent = ifelse(incumbent == "I", 1, 0))
+}
+
 ## Join with candidate ideology and incumbency data
-box_auth()
-# Read 
-nominate_data = box_read_csv(file_id = '291617511779') %>%
-    select(os_id, nominate_dim1) %>%
-    rename(candidate = os_id, ideology = nominate_dim1)
-
-# Read 'VLC_16_full.csv' from box
-candidate_meta_data = box_read_csv(file_id = '255302928759', fread = TRUE) %>%
-    dplyr::select(Actor_ID, Incum) %>%
-    rename(candidate = Actor_ID, incumbent = Incum) %>%
-    mutate(incumbent = ifelse(incumbent == "I", 1, 0))
-
-simulation_cut = left_join(simulation_cut, nominate_data) %>% 
+simulation_cut = left_join(simulation_cut, nominate_data) %>%
     left_join(candidate_meta_data)
 
 # Proportion of contributions to each decile of the ideology distribution
@@ -157,7 +169,7 @@ ggplot(pdat, aes(x = ideology_bin, color = network_type)) +
     theme(axis.text.x = element_blank(),
           axis.ticks.x = element_blank()) +
     ylab('Within group proportion') + xlab('Ideology')
-ggsave('../paper/figures/donations_ideology_norm_by_all_donations.png', 
+ggsave('paper/figures/donations_ideology_norm_by_all_donations.png', 
        width = pe$p_width, height = 0.7 * pe$p_width)
 
 
@@ -183,6 +195,10 @@ dd = as.data.frame(donation_cascades) %>%
               prop = n / nrow(.)) %>%
     mutate(incumbent = as.factor(incumbent))
 
+pdat$network_type[pdat$network_type == 'directional_networks'] = 'directional'
+pdat$network_type[pdat$network_type == 'spatial_networks'] = 'spatial'
+pdat$network_type[pdat$network_type == 'netinf_network'] = 'netinf'
+
 ggplot(pdat, aes(x = network_type, y = prop, color = incumbent,
                  linetype = incumbent)) +
     geom_boxplot() +
@@ -192,6 +208,7 @@ ggplot(pdat, aes(x = network_type, y = prop, color = incumbent,
                        labels = c('Non-Incumbent', 'Incumbent')) +
     scale_linetype(name = "", labels = c('Non-Incumbent', 'Incumbent')) +
     pe$theme + ylab('Proportion') + xlab('Network Type') +
-    facet_wrap(~proportion_observed)
+    facet_wrap(~proportion_observed) +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1))
 ggsave('../paper/figures/donations_incumbent.png', 
        width = pe$p_width, height = 0.7 * pe$p_width)
