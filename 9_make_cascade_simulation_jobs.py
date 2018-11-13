e77d08742a3b82bb43583332b85f255dc65cf62e
flinder <fridolin.linder@gmail.com>
2018-11-13 18:16:42 -0500

    check for total jobs in 9_

--- 9_make_cascade_simulation_jobs.py
+++ 9_make_cascade_simulation_jobs.py
@@ -15,8 +15,10 @@ def submit_job(job_id, template, queue, n_per_job):
         try:
             ntry += 1
             response = subprocess.getoutput("qsub {}".format(jobname))
+            print(response)
 
         except subprocess.CalledProcessError as error:
+            print('error')
             # Raise exception and halt program after 5 unsuccesful tries 
             if ntry > 5:
                 raise
@@ -29,25 +31,28 @@ def submit_job(job_id, template, queue, n_per_job):
 
 if __name__ == '__main__':
 
-    queues = [{'name': 'bbd5087_a_g_hc_default', 'n_jobs': 34}, 
-              {'name': 'bbd5087_a_g_sc_default', 'n_jobs': 20}, 
-              {'name': 'bbd5087_b_g_sc_default', 'n_jobs': 20}]
+    queues = [
+            #{'name': 'bbd5087_a_g_hc_default', 'n_jobs': 76}, 
+            #{'name': 'bbd5087_a_g_sc_default', 'n_jobs': 36}, 
+            {'name': 'bbd5087_b_g_sc_default', 'n_jobs': 74}
+              ]
 
-    total_cores = 74
+    total_cores = sum([x['n_jobs'] for x in queues])
     n_candidates = 740
-    candidates_per_job = 10
+    candidates_per_job = n_candidates / total_cores
+    if total_cores % 1 != 0:
+        raise ValueError('total_cores needs to be a factor of n_candidates')
 
     with open('cascade_simulation_job_template.pbs') as infile:
         template = infile.read()
     
     job_id = 0
     for queue in queues:
-
         for i in range(queue['n_jobs']):
-
             job_id += 1 # 1-indexed for R
+            print(f'Submitting job {job_id}')
             q_name = queue['name']
             q = f'#PBS -A {q_name}'
             submit_job(job_id, template, q, candidates_per_job)
-            time.sleep(0.2)
+            time.sleep(0.1)
 
